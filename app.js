978be459ab9786b350aed06cd40c0c865b955c1f
2WWWEEE <luckojanova10@gmail.com>
2025-11-26 18:05:13 +0300

    final version

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
- console.log('hello world');
+let firstName = 'Наталья';
+console . log('firstName');
+
+let age = 49
+console . log('Мой возраст:'+'age'+'лет');
+
+let city = 'Нижний Новгород';
+console . log('city');
+
+let hobby = 'дизайн';
+console . log('hobby');
